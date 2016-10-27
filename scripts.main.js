6d9877e40b0dd34b42de487bfff8c273b1f8ade4
Riochanyade <nittario@net77-dhcp69.sfc.keio.ac.jp>
2016-10-27 11:08:55 +0900

    index.html 保存

---
+++ scripts.main.js
@@ -0,0 +1,38 @@
+function isEvenNumber(n){
+　　return n % 2 ==0;
+}
+
+function toEvenNumber(number){
+    if(isEvenNumber(number)){
+        return number;
+    }
+    return number + 1;
+}
+
+functon toEvenNumber(number){
+    let result;
+    if(number % 2 ==0){
+        result = number;
+    }else{
+        result = number + 1;
+    }
+    return result;
+}
+
+return number * 2;
+
+
+
+
+function toOddNumber(number){
+    return isEvenNumber + 1;
+}
+
+
+
+function toOddNumber(number){
+    if(isEvenNumber(number)){
+        return number + 1;
+    }
+    return number
+}
